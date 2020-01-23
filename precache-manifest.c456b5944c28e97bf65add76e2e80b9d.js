b8a6f9d387b566a0cc7f3e527ea5c67465e07fe3
Zedwag <zedwag@gmail.com>
2020-01-23 04:14:26 +0300

    Updates

---
+++ precache-manifest.c456b5944c28e97bf65add76e2e80b9d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "902c951ff74ffdbad8a5e2648891c640",
+    "url": "/tmp/index.html"
+  },
+  {
+    "revision": "702f535ef98029d5cc6e",
+    "url": "/tmp/static/js/2.ae7dd821.chunk.js"
+  },
+  {
+    "revision": "2a64065ee5003119ffb2",
+    "url": "/tmp/static/js/main.5de27fc2.chunk.js"
+  },
+  {
+    "revision": "0556aa14b242c21e5cb6",
+    "url": "/tmp/static/js/runtime-main.0ede146c.js"
+  }
+]);
